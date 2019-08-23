b2c56355600558657e8dac8a549e401fd59e20e3
wangfeng <827326857@qq.com>
2019-08-23 10:55:12 +0800

    初始化

---
+++ coreML/PlantIdentificationVC.swift
@@ -0,0 +1,24 @@
+//
+//  PlantIdentificationVC.swift
+//  coreML
+//
+//  Created by wangfeng on 2019/8/23.
+//  Copyright © 2019 wangfeng. All rights reserved.
+//
+
+import UIKit
+
+class PlantIdentificationVC: UIViewController {
+
+    @IBOutlet weak var startBut: UIButton!
+    @IBOutlet weak var selectImageV: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func OnSelectClick(_ sender: UIButton) {
+    }
+    
+}
---
+++ coreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coreML
+//
+//  Created by wangfeng on 2019/8/23.
+//  Copyright © 2019 wangfeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
